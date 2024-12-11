f1a4353fac4c0cfe1b362839458fc0ae88e43ea1
Vanekprofi <vanekprofi99@gmail.com>
2024-12-12 00:42:01 +0500

    +_move

--- 15.py
+++ 15.py
@@ -34,3 +34,63 @@ correct_board = board[:]
 shuffle(board)
 draw_board()
 root.mainloop()
+
+from tkinter import Canvas, Tk
+from random import shuffle
+
+BOARD_SIZE = 4
+SQUARE_SIZE = 80
+EMPTY_SQUARE = BOARD_SIZE ** 2
+
+root = Tk()
+root.title("Пятнашки")
+
+c = Canvas(root, width=BOARD_SIZE * SQUARE_SIZE,
+           height=BOARD_SIZE * SQUARE_SIZE, bg='#808080')
+c.pack()
+
+def draw_board():
+    c.delete('all')
+    for i in range(BOARD_SIZE):
+        for j in range(BOARD_SIZE):
+            index = str(board[BOARD_SIZE * i + j])
+            if index != str(EMPTY_SQUARE):
+                c.create_rectangle(j * SQUARE_SIZE, i * SQUARE_SIZE,
+                                   j * SQUARE_SIZE + SQUARE_SIZE,
+                                   i * SQUARE_SIZE + SQUARE_SIZE,
+                                   fill='#FFFFFF',
+                                   outline='#000000')
+                c.create_text(j * SQUARE_SIZE + SQUARE_SIZE / 2,
+                              i * SQUARE_SIZE + SQUARE_SIZE / 2,
+                              text=index,
+                              font="Arial {} italic".format(int(SQUARE_SIZE / 4)),
+                              fill='#000000')
+
+def get_empty_neighbor(index):
+    empty_index = board.index(EMPTY_SQUARE)
+    abs_value = abs(empty_index - index)
+    if abs_value == BOARD_SIZE:
+        return empty_index
+    elif abs_value == 1:
+        max_index = max(index, empty_index)
+        if max_index % BOARD_SIZE != 0:
+            return empty_index
+    return index
+
+
+def click(event):
+    x, y = event.x, event.y
+    x = x // SQUARE_SIZE
+    y = y // SQUARE_SIZE
+    board_index = x + (y * BOARD_SIZE)
+    empty_index = get_empty_neighbor(board_index)
+    board[board_index], board[empty_index] = board[empty_index], board[board_index]
+    draw_board()
+
+c.bind('<Button-1>', click)
+c.pack()
+
+board = list(range(1, EMPTY_SQUARE + 1))
+shuffle(board)
+draw_board()
+root.mainloop()
